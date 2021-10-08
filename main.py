70bbd8c6b5f3adbce167d3a2d245491375d159d3
bram.willems <brammeke20142014@gmail.com>
2021-10-08 23:18:15 +0100

    First tests

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print('Hi! Welcome to the Roll optimizer!')
+print('ATF = Add To Face Value')
+
+inputAmount = 4
+
+currentShop = []
+
+def Inputs():
+    userInput = str(input("What kind of upgrade?")).upper()
+    if userInput == "ATF":
+        ATFInput()
+    else:
+        Inputs()
+
+def ATFInput():
+    currentShop.append(["ATF", int(input("What is the number that is going to be added to the face?")), int(input("What is the cost of the upgrade?"))])
+
+for i in range(inputAmount):
+    Inputs()
+
+print(currentShop)
